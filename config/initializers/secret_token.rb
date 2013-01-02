ffaec0548d327e85de45a5336f3af138f83276cd
Morgan McDaris <morganmcdaris@yahoo.com>
2013-01-02 10:57:50 -0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devdiary::Application.config.secret_token = '6aecc9ca2b5d23b138bcc6b531a628ade10351aac5aac7e70543413fa7302b3dba44628706710a25554e2fe899d48f598abfa90cf83cc3c6e9029cf0abdeda4d'
